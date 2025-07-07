da3635acdec965ceb3cc32666cd8f0cf3c4d54cf
Jake Stone <jakestone@MacBook-Air-4.local>
2025-07-07 13:55:41 -0700

    Hooked up homepage to root URL

--- tracker/views.py
+++ tracker/views.py
@@ -234,5 +234,5 @@ def view_cme_pdf(request, cme_id):
 
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'tracker/home.html')
+def homepage(request):
+    return render(request, "home.html")
